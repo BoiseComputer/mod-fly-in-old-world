8922f18a3b430584ba3eaa67a0d63aec9e45bc7f
Brian Aldridge <brian@aldridge.house>
2025-04-24 12:52:52 -0600

    feat: add Old World Flying module configuration and functionality

--- src/FlyInOldWorld.cpp
+++ src/FlyInOldWorld.cpp
@@ -2,29 +2,28 @@
  * Copyright (C) 2016+ AzerothCore <www.azerothcore.org>, released under GNU AGPL v3 license: https://github.com/azerothcore/azerothcore-wotlk/blob/master/LICENSE-AGPL3
  */
 
+#include "Config.h"
 #include "ScriptMgr.h"
 #include "Player.h"
 
-enum {
-    OLD_WORLD_FLYING_SPELL = 200001
-};
-
-// Add player scripts
 class FlyInOldWorld : public PlayerScript
 {
 public:
-    FlyInOldWorld() : PlayerScript("FlyInOldWorld") { }
+    FlyInOldWorld() : PlayerScript("FlyInOldWorld") {}
 
-    bool OnCanPlayerFlyInZone(Player* player, uint32 mapId, uint32 zoneId, SpellInfo const* bySpell) override
+    bool OnCanPlayerFlyInZone(Player *player, uint32 mapId, uint32 zoneId, SpellInfo const *bySpell) override
     {
+        if (!sConfigMgr->GetBoolDefault("ModOldWorldFlying.Enable", true))
+            return false;
+
+        uint32 spellId = sConfigMgr->GetIntDefault("ModOldWorldFlying.SpellId", 200001);
         uint32 v_map = GetVirtualMapForMapAndZone(mapId, zoneId);
         if (v_map == 0 || v_map == 1)
         {
-            if (!player->HasSpell(OLD_WORLD_FLYING_SPELL))
+            if (!player->HasSpell(spellId))
             {
                 return false;
             }
-            
         }
         return true;
     }
